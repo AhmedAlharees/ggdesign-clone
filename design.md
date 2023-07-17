19edfa2cb03ba0c5a32ce3d1e9225937b751b295
Ahmed Alharees <ahmedjoad.2014@gmail.com>
2023-07-17 17:54:29 +0300

    first commit

---
+++ design.md
@@ -0,0 +1,17 @@
+# This is a clown for the ggdesing website
+
+## colors
+ $background-color: #171717
+ $font-color: #dbd7cb
+ $shift-color: #0173fc
+ $light-main: #d2cfc6
+
+## font
+font-family: 'Rubik', sans-serif;
+
+## GSAP CDN 
+ - CDN for GSAP main tools
+ <script src="https://cdnjs.cloudflare.com/ajax/libs/gsap/3.12.2/gsap.min.js" integrity="sha512-16esztaSRplJROstbIIdwX3N97V1+pZvV33ABoG1H2OyTttBxEGkTsoIVsiP1iaTtM8b3+hu2kB6pQ4Clr5yug==" crossorigin="anonymous" referrerpolicy="no-referrer"></script>
+
+ - CDN for GSAP scrollTrigger
+ <script src="https://cdnjs.cloudflare.com/ajax/libs/gsap/3.12.2/ScrollTrigger.min.js" integrity="sha512-Ic9xkERjyZ1xgJ5svx3y0u3xrvfT/uPkV99LBwe68xjy/mGtO+4eURHZBW2xW4SZbFrF1Tf090XqB+EVgXnVjw==" crossorigin="anonymous" referrerpolicy="no-referrer"></script>
---
+++ script.js
@@ -0,0 +1,87 @@
+const header = document.querySelector("header");
+const introHeader = document.querySelector(".intro__header");
+const pricingHeader = document.querySelector(".price-head");
+const handHeader = document.querySelector(".hand__header");
+const priceCards = document.querySelector('.price-card');
+const imageContainer = document.querySelector('.moving-images');
+console.log(handHeader);
+
+const myText = new SplitType(introHeader, { type: "chars", charClass: "introChars" });
+const myText2 = new SplitType(pricingHeader, {type: 'chars', charClass: 'pricingChars'})
+const myText3 = new SplitType(handHeader, {type: 'chars', charClass: 'handChars'})
+gsap.registerPlugin(ScrollTrigger);
+
+const observer = new IntersectionObserver((entries) => {
+  entries.forEach((entry) => {
+    if (entry.target.className == 'intro__header' && entry.isIntersecting) {
+      gsap.to('.introChars', {
+        opacity: 1,
+        y: 0,
+        stagger: 0.019,
+        duration: 0.4,
+      });
+    }
+    if (entry.target.className == 'price-head' && entry.isIntersecting){
+      gsap.to('.pricingChars',{
+        opacity: 1,
+        y: 0,
+        stagger: 0.019,
+        duration: .7,
+      })
+    }
+    if (entry.target.className == 'price-card' && entry.isIntersecting){
+      gsap.to('.price-card',{
+        scale: 1,
+        opacity: 1,
+        delay: .01,
+        stagger: .1,
+        duration: 1,
+      })
+    }
+    if (entry.target.className == 'hand__header' && entry.isIntersecting){
+      gsap.to('.handChars', {
+        opacity: 1,
+        y: 0,
+        stagger: 0.1,
+        duration: 1,
+      })
+    }
+  });
+});
+
+
+gsap.to('.left-cloud', {
+  x: 0,
+  duration: 2,
+  scrollTrigger:{
+    trigger: imageContainer,
+    scrub: true,
+    start: 'top 80%',
+    end: "top 20%",
+    markers: true
+  }
+})
+gsap.to('.right-cloud', {
+  x: 0,
+  duration: 2,
+  scrollTrigger:{
+    trigger: imageContainer,
+    scrub: true,
+    start: 'top 80%',
+    end: "top 20%"
+  }
+})
+
+gsap.to('body', {
+  backgroundColor: '#0173fc',
+  duration: 0.1,
+  scrollTrigger: {
+    trigger: imageContainer,
+    scrub: true
+  }
+})
+
+observer.observe(introHeader);
+observer.observe(pricingHeader)
+observer.observe(priceCards)
+observer.observe(handHeader)
